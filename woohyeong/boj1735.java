81fc4c1203e8c53f7014024139b004d5cf5d0685
wxxhyeong <wxxhyeong@gmail.com>
2023-02-23 11:53:46 +0900

    테스트2

--- woohyeong/boj1735.java
+++ woohyeong/boj1735.java
@@ -4,5 +4,6 @@ public class boj1735 {
     public static void main(String[] args) {
         System.out.println("테스트");
         System.out.println("테스트2");
+        System.out.println(",");
     }
 }
