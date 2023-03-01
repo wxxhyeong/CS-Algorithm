8629c59e44ea3e023a61a966fe336e156fe1ddfd
wxxhyeong <wxxhyeong@gmail.com>
2023-03-01 15:02:21 +0900

    boj5376(소수를 분수로)/3월1일
    쉬운데 자바로 풀려니 복잡

--- woohyeong/boj5376.java
+++ woohyeong/boj5376.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class boj5376 {
+
+    static long gcd(long a1, long b1) {
+
+
+        if (a1 % b1 == 0)
+            return b1;
+        else
+            return gcd(b1, a1 % b1);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -12,51 +21,53 @@ public class boj5376 {
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            int start, end;
+            boolean isRepeat = false;
+            int start = 0;
             StringBuilder sbNum = new StringBuilder();
 
             String input = br.readLine();
-            input = input.substring(2, input.length());
+            input = input.substring(2);
 
             for (int k = 0; k < input.length(); k++) {
-                if (
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            for (int j = 0; j < input.length(); j++) {
-                if (input.charAt(j) == '(') {
-                    start = j + 1;
-                } else if (input.charAt(j) == ')') {
-                    end = j + 1;
-                } else {
-                    sbNum.append(input.charAt(j));
+                if (input.charAt(k) == '(') {
+                    start = k;
+                    isRepeat = true;
+                }
+                else if (input.charAt(k) != ')'){
+                    sbNum.append(input.charAt(k));
                 }
             }
 
-            System.out.println(sbNum);
+            //System.out.println("sbNum = " + sbNum);
+            //System.out.println("start = " + start);
+            long sN; //분자
+            long pN; //분모
+            if (start == 0 && !isRepeat) {
+                sN = Long.parseLong(sbNum.toString());
+                pN = (long)Math.pow(10, sbNum.length());
 
-
+            }
+            else {
+                if (start == 0) {
+                    sN = Long.parseLong(sbNum.toString());
+                }
+                else {
+                    sN = Long.parseLong(sbNum.toString()) - Long.parseLong(sbNum.substring(0, start));
+                }
+                pN = (long) Math.pow(10, sbNum.length()) - (long) Math.pow(10, start);
+            }
+            //System.out.println("sN = " + sN);
+            //System.out.println("pN = " + pN);
+            
+            long gcd = gcd(sN, pN);
+            //System.out.println("gcd = " + gcd);
+            sN /= gcd;
+            pN /= gcd;
+            //sb.append(sN + "/" + pN +"\n");
+            System.out.println(sN + "/" + pN );
 
         }
+
+        //System.out.println(sb.toString());
     }
 }
