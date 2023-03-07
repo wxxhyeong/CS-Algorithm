78f9aadb056f7487ebdc7b5684dacf3bfdf2b6a6
wxxhyeong <wxxhyeong@gmail.com>
2023-03-07 14:54:17 +0900

    boj1837(암호제작)/3월7일/못품(합동식)

---
+++ woohyeong/boj1837.java
@@ -0,0 +1,20 @@
+package woohyeong;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1837 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        String number = stk.nextToken();
+        int k = Integer.parseInt(stk.nextToken());
+
+
+
+    }
+}
